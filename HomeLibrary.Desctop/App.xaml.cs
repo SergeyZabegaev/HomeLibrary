83a81ad2523f4f1c86e280ceb9c0f45346cd7b25
Zabegaev Sergey <zabegaev062015@gmail.com>
2025-05-07 18:23:39 +0300

    Initial commit

---
+++ HomeLibrary.Desctop/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace HomeLibrary.Desctop;
+
+public partial class App : Application
+{
+}
---
+++ HomeLibrary.Desctop/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace HomeLibrary.Desctop;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
