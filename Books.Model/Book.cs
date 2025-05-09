bc4f7589cf6299335cca96a7ad62c0d4d22635a8
Zabegaev Sergey <zabegaev062015@gmail.com>
2025-05-09 15:50:22 +0300

    Добавление проекта Model

---
+++ Books.Model/Book.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Books.Model;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    public required string Title { get; set; }
+    public string Author { get; set; }
+    public string? Description { get; set; }
+    public string Category { get; set; }
+    public int PageCount { get; set; }
+    public DateTime PublishedDate { get; set; }
+
+}
